aba19b547859b5af47008cec5b5d36aa8d686b6d
Alexa Ruiz <ruizmalexa@gmail.com>
2023-03-14 15:22:40 +0000

    Fixed syntax

--- RB100/ruby_book/variables/exercise_5.rb
+++ RB100/ruby_book/variables/exercise_5.rb
@@ -1,7 +1,7 @@
 =begin
 look at the following programs:
 
-x=0
+x = 0
 3.times do
   x += 1
 end 
@@ -19,7 +19,7 @@ puts x
 what does x print to the screen in each case? Do they both give erros? Are the 
 erros different? why?
 
-Answer: In the first program, x will be equal to 3 because this a block, and the
+Answer: In the first program, x will be equal to 1 because this a block, and the
 inner scope can access variables initialized in an outer scope. The second 
 program is also a block, but the outer scope is trying to access a variable that
 is initialized in the inner scope, which is not possible. The second program will
