74366937bd85f5967a03dd9d353cc05e394cb366
Alexa Ruiz <ruizmalexa@gmail.com>
2023-03-23 20:11:37 +0000

    Refactored if/else statement

--- RB100/ruby_book/flow_control/exercise_3.rb
+++ RB100/ruby_book/flow_control/exercise_3.rb
@@ -1,18 +1,17 @@
 =begin
 write a program that takes a number from the user beween 0 and 100 and reports
 back whether the number is between 0 and 51, 51 and 100, or above 100.
+=end
 
-
-puts "Please input a number"
+puts "Please input a number between 0 and 100"
 number = gets.chomp.to_i
 
-if number > 0 && number < 51
-  puts "#{number} is between 0 and 51."
-elsif number >= 51 && number < 101
-  puts "#{number} is between 50 and 101."
-elsif number > 100
-  puts "#{number} is greater than 100"
+if number < 0
+  puts "#{number} is negative"
+elsif number <= 50
+  puts "#{number} is between 0 and 50"
+elsif number <= 100
+  puts "#{number} is between 0 and 100"
 else
-  puts "#{number} is negative."
+  puts "#{number} is greater than 100"
 end
-=end
