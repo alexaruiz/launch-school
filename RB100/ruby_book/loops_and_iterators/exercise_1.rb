e1c6b86b9f9a4a39cc5e6758cf91331f0635ade6
Alexa Ruiz <ruizmalexa@gmail.com>
2023-03-23 21:00:42 +0000

    Fixed logic error in answer

--- RB100/ruby_book/loops_and_iterators/exercise_1.rb
+++ RB100/ruby_book/loops_and_iterators/exercise_1.rb
@@ -1,13 +1,15 @@
 =begin
 what does the each method in the following program return after it is finished
 executing?
+=end
 
 x = [1, 2, 3, 4, 5]
 x.each do |a|
   a + 1
 end
 
+=begin
 for each element in the array of integers from 1 to 5, add 1 to the element. The
-each method will return the original array of 1 to 5 (i.e., it will not mutate
-the array. For this, you would need to use the collect or map method.)
+each method will return the original array of 1 to 5, unlike the map method which
+would return a new array with the incremented values)
 =end 
