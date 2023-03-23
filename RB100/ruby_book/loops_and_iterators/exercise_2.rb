fa86b10cff16534af1380d6b104432edb24e4a0c
Alexa Ruiz <ruizmalexa@gmail.com>
2023-03-23 21:01:32 +0000

    Added do/while alternative

--- RB100/ruby_book/loops_and_iterators/exercise_2.rb
+++ RB100/ruby_book/loops_and_iterators/exercise_2.rb
@@ -1,6 +1,7 @@
 =begin
 write a while loop that takes input fom the user, performs an action, and only
 stops when the user types "STOP". Each loop can get info from the user.
+=end 
 
 puts "Please type something:"
 input = gets.chomp
@@ -9,4 +10,10 @@ while input != "STOP"
   puts "Type 'STOP' or else I will continue prompting for input"
   input = gets.chomp
 end
-=end
+
+#alternative solution
+loop do
+  puts "type something"
+  input = gets.chomp
+  break if input == "STOP"
+end
