b666db4b99538687de9daf6552735d4db1c307bd
Alexa Ruiz <ruizmalexa@gmail.com>
2023-03-23 20:08:50 +0000

    Fixed error on line 27

--- RB100/ruby_book/flow_control/exercise_4.rb
+++ RB100/ruby_book/flow_control/exercise_4.rb
@@ -2,33 +2,31 @@
 what will each block of code print to the screen? Write yor answer on a piece of
 paper or in a text editor and then run each block of code to see if you were 
 correct.
+=end
 
+#Snippet 1
+"4" == 4 ? puts("TRUE") : puts("FALSE") 
+#false (different variable types)
 
-Snippet 1
-"4" == 4 ? puts("TRUE") : puts("FALSE") #false (different variable types)
-
-Snippet 2
+#Snippet 2
 x = 2
 if ((x * 3) / 2) == (4 + 4 - x - 3)
   puts "Did you get it right?"
 else
   puts "Did you?"
 end
-
 #did you get it right?
 
-Snippet 3
+#Snippet 3
 y = 9
 x = 10
-if (x+1) <= (y)
+if (x + 1) <= (y)
   puts "Alright."
 elsif (x + 1) >= (y)
   puts "Alright now!"
-else if (y + 1) == x
+elsif (y + 1) == x
   puts "ALRIGHT NOW!"
 else 
   puts "Alrighty!"
 end
-
 #Alright now!
-=end
