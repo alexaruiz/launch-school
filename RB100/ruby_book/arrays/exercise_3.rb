9efaca55e72e8448e96766e02fd7e06ddae5cbcf
Alexa Ruiz <ruizmalexa@gmail.com>
2023-03-24 19:06:58 +0000

    Added alternative solution

--- RB100/ruby_book/arrays/exercise_3.rb
+++ RB100/ruby_book/arrays/exercise_3.rb
@@ -1,9 +1,10 @@
-=begin
-how do you return the word "example" from the following array?
+#how do you return the word "example" from the following array?
 
 
 arr = [["test", "hello", "world"], ["example", "mem"]]
 
 arr.flatten.each { |element| puts element if element == "example" }
-=end
+
+#alternative solution
+puts arr.last.first
 
--- RB100/ruby_book/arrays/exercise_8.rb
+++ RB100/ruby_book/arrays/exercise_8.rb
@@ -4,8 +4,18 @@ result of incrementing each value in the original array by a value of 2. You
 should have two arrays at the end of this program: the original array and the 
 new array you've created. Print both arrays to the screen using the p method
 instead of puts.
+=end
 
 original_array = [0, 1, 2, 3, 4, 5, 6]
 new_array = []
 original_array.each { |element| new_array << element + 2 }
-=end
+
+p new_array
+
+#alternative solution
+original_array.each do |element|
+  element += 2
+  new_array << element
+end 
+
+p new_array
